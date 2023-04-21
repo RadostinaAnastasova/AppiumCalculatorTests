06ec4c7d036e90cdd31c3933c47b90cc27388c70
Радостина Анастасова <ina_rd@abv.bg>
2023-04-21 17:03:16 +0300

    first commit

---
+++ CalculatorTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AppiumCalculatorTests
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
