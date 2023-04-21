09a554a6397efc5b2234f5a607ac5ff022ba6e3f
Радостина Анастасова <ina_rd@abv.bg>
2023-04-21 17:29:12 +0300

    setup

--- CalculatorTests.cs
+++ CalculatorTests.cs
@@ -1,12 +1,30 @@
 using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
 
 namespace AppiumCalculatorTests
 {
     public class CalculatorTests
     {
+        private const string appiumServer = "http://127.0.0.1:4723/wd/hub";
+        private const string appLocation = @"C:\\R\\QA\\QA Automation FrontEnd\\04.Appium-Desktop-Testing-Resources\\SummatorDesktopApp.exe";
+        private WindowsDriver<WindowsElement> driver;
+        private AppiumOptions appiumOptions;
+
         [SetUp]
-        public void Setup()
+        public void OpenApplication()
         {
+            this.appiumOptions= new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", appLocation);
+            appiumOptions.AddAdditionalCapability("PlatformName", "Windows");
+            this.driver = new WindowsDriver<WindowsElement>(new Uri(appiumServer), appiumOptions);
+        }
+
+        [TearDown]
+        public void CloseApplication()
+        {
+            this.driver.Quit();
+            //driver.Quit();
         }
 
         [Test]
